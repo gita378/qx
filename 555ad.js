4e527bd142496195065f83887a56e1bffd1dee44
gita378 <115575983+gita378@users.noreply.github.com>
2022-11-06 13:31:12 +0800

    Create 555ad.js

---
+++ 555ad.js
@@ -0,0 +1,26 @@
+/*******************************
+  公众号:木木IOS分享
+关注了解更多新科技！！！
+去广告
+脚本名称:去广告
+使用声明：️此脚本仅供学习与交流，
+        请勿转载与贩卖！️️️
+群1077223830
+*******************************
+[rewrite_local]
+^http[s]?:\/\/vip7.fzwdyy.cn\/api\/getAdvertInfo.+$ url script-response-body fzwdyy.js
+[mitm] 
+hostname = *.fzwdyy.*
+*******************************
+Surge
+
+[Script]
+^http[s]?:\/\/vip7.fzwdyy.cn\/api\/getAdvertInfo.+$ requires-body=1,max-size=0,script-path=fzwdyy.js
+
+[MITM]
+hostname = *.fzwdyy.*
+
+*******************************/
+var obj = JSON.parse($response.body);
+    obj.data.= ;
+    $done({body: JSON.stringify(obj)});
