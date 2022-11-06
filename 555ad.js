35ff352f6e4fb2c38fac2e00b8dad7d783817eb0
gita378 <115575983+gita378@users.noreply.github.com>
2022-11-06 13:39:50 +0800

    Update 555ad.js

--- 555ad.js
+++ 555ad.js
@@ -6,13 +6,12 @@
 使用声明：️此脚本仅供学习与交流，
         请勿转载与贩卖！️️️
 群1077223830
-*******************************
+*******************************/
 [rewrite_local]
 ^http[s]?:\/\/vip7.fzwdyy.cn\/api\/getAdvertInfo.+$ url script-response-body 555ad.js
 [mitm] 
 hostname = *.fzwdyy.*
 
-*******************************/
 var obj = JSON.parse($response.body);
     obj.data.= ;
     $done({body: JSON.stringify(obj)});
